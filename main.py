4cc37a75450041c9b6ee011edbde5f64ddd2c489
Avdeenok Alexey <alex_avdeenok@mail.ru>
2024-10-12 00:45:38 +0700

    pylint 10

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
+"""
+Модуль реализует окрашивание подстрок в строке
+"""
 import argparse
 import os
 import time
-from search import search
 from colorama import init, Fore, Style, Back
+from search import search
 
 
 def log_execution_time(func):
+    """
+    Логирование времени выполнения
+    :param func: функция
+    :return:
+    """
     def wrapper(*args, **kwargs):
-        start_time = time.perf_counter()  # Точнее замер начального времени
+        start_time = time.perf_counter()
         result = func(*args, **kwargs)
-        end_time = time.perf_counter()  # Точнее замер времени после выполнения
+        end_time = time.perf_counter()
         execution_time = end_time - start_time
-        print(f"Function '{func.__name__}' executed in {execution_time:.8f} seconds")
+        print(f"Function '{func.__name__}' executed in "
+              f"{execution_time:.8f} seconds")
         return result
     return wrapper
 
 
-# Создание парсера для аргументов командной строки
 def create_parser():
-    parser = argparse.ArgumentParser(description="Утилита для поиска подстрок в строке с различными параметрами")
+    """
+    Создание парсера для строки аргументов
+    :return:
+    """
+    parser = argparse.ArgumentParser(description="Утилита для поиска "
+                                                 "подстрок в строке с "
+                                                 "различными параметрами")
 
     # Аргумент для исходной строки
     parser.add_argument(
@@ -41,7 +55,8 @@ def create_parser():
     parser.add_argument(
         "-cs", "--case-sensitivity",
         action='store_true',
-        help="Чувствительность к регистру (по умолчанию - нечувствительный поиск)."
+        help="Чувствительность к регистру "
+             "(по умолчанию - нечувствительный поиск)."
     )
 
     # Аргумент для метода поиска (с начала или с конца строки)
@@ -50,14 +65,16 @@ def create_parser():
         type=str,
         choices=['first', 'last'],
         default='first',
-        help="Метод поиска: 'first' для поиска с начала, 'last' для поиска с конца."
+        help="Метод поиска: 'first' для поиска с начала, "
+             "'last' для поиска с конца."
     )
 
     # Аргумент для ограничения количества совпадений
     parser.add_argument(
         "-c", "--count",
         type=int,
-        help="Количество совпадений, которые нужно найти (по умолчанию - все совпадения)."
+        help="Количество совпадений, "
+             "которые нужно найти (по умолчанию - все совпадения)."
     )
 
     # Аргумент для указания пути к файлу
@@ -73,29 +90,36 @@ def create_parser():
         "-l", "--limit",
         type=int,
         default=10,
-        help="Ограничение на количество выводимых строк (по умолчанию - 10 строк)."
+        help="Ограничение на количество выводимых строк "
+             "(по умолчанию - 10 строк)."
     )
 
     return parser
 
 
-# Функция для чтения файла, если указан путь к файлу
 def read_file(file_path, line_limit=10):
+    """
+    Функция для чтения текста из фала
+    :param file_path: путь к файлу
+    :param line_limit: максимальное число строк
+    :return: строка
+    """
     if not os.path.isfile(file_path):
         raise FileNotFoundError(f"Файл {file_path} не найден.")
     with open(file_path, 'r', encoding='utf-8') as file:
-        lines = file.readlines()  # Читаем все строки файла
+        lines = file.readlines()
 
-        # Ограничиваем количество строк до line_limit
         limited_lines = lines[:line_limit]
 
-        # Соединяем ограниченные строки в одну строку
         return ''.join(limited_lines)
 
 
-# Главная функция, которая получает аргументы и вызывает функцию поиска
 @log_execution_time
 def main():
+    """
+    Основная функция которая реализует окрашивание строки
+    :return:
+    """
     parser = create_parser()
     args = parser.parse_args()
 
@@ -108,7 +132,8 @@ def main():
             return
     else:
         if args.string is None:
-            raise ValueError("Необходимо указать либо строку, либо путь к файлу.")
+            raise ValueError("Необходимо указать либо строку, "
+                             "либо путь к файлу.")
         target_string = args.string
 
     # Параметры для поиска
@@ -116,12 +141,12 @@ def main():
     case_sensitivity = args.case_sensitivity
     method = args.method
     count = args.count
-    limit = args.limit
 
     init()
 
     if len(substrings) == 1:
-        tuple_subs = search(target_string, substrings[0], case_sensitivity, method, count)
+        tuple_subs = search(target_string, substrings[0],
+                            case_sensitivity, method, count)
         if tuple_subs is None:
             print(target_string)
         else:
@@ -131,11 +156,13 @@ def main():
                 list_of_subs.append(tuple(mas))
             print(color_text(target_string, list_of_subs))
     else:
-        dictionary = search(target_string, substrings, case_sensitivity, method, count)
+        dictionary = search(target_string, substrings,
+                            case_sensitivity, method, count)
         if dictionary is None:
             print(target_string)
         else:
-            print(color_text_many(target_string, make_tuple_of_subs(dictionary)))
+            print(color_text_many(target_string,
+                                  make_tuple_of_subs(dictionary)))
 
 
 def color_text(text, highlights):
@@ -143,34 +170,41 @@ def color_text(text, highlights):
     Окрашивает подстроки в строке по заданным параметрам.
 
     :param text: исходная строка
-    :param highlights: список кортежей формата (индекс начала подстроки, длина подстроки, цвет)
+    :param highlights: список кортежей формата
+    (индекс начала подстроки, длина подстроки, цвет)
     :return: строка с окрашенными подстроками
     """
     colored_text = ""
     last_index = 0
 
-    # Сортируем по индексу начала подстроки, чтобы корректно обрабатывать перекрытия
     highlights = sorted(highlights, key=lambda x: x[0])
 
     for start, length, color in highlights:
-        # Добавляем неокрашенную часть строки
         colored_text += text[last_index:start]
-        # Добавляем окрашенную подстроку
         colored_text += color + text[start:start + length] + Style.RESET_ALL
-        # Обновляем индекс последней обработанной позиции
         last_index = start + length
 
-    # Добавляем оставшуюся часть строки, если она есть
     colored_text += text[last_index:]
 
     return colored_text
 
 
 def make_tuple_of_subs(dictionary):
+    """
+    Создает вспомогательный список кортежей формата
+    (индекс, подстрока, цвет)
+    :param dictionary: словарь
+    :return: список кортежей
+    """
     list_of_tuples = []
-    colors = [Fore.GREEN, Fore.RED, Fore.YELLOW, Fore.BLUE, Fore.BLACK + Back.WHITE, Fore.CYAN,
-              Fore.MAGENTA, Fore.RED + Back.WHITE, Fore.YELLOW + Back.WHITE, Fore.GREEN + Back.WHITE,
-              Fore.MAGENTA + Back.WHITE]
+    colors = [Fore.GREEN, Fore.RED, Fore.YELLOW, Fore.BLUE,
+              Fore.BLACK + Back.WHITE, Fore.CYAN,
+              Fore.MAGENTA, Fore.RED + Back.WHITE, Fore.YELLOW + Back.WHITE,
+              Fore.GREEN + Back.WHITE,
+              Fore.MAGENTA + Back.WHITE, Fore.RED + Back.YELLOW,
+              Fore.BLACK + Back.YELLOW,
+              Fore.GREEN + Back.RED, Fore.GREEN + Back.YELLOW,
+              Fore.CYAN + Back.RED]
     index_colors = 0
     for key, value in dictionary.items():
         if value is None:
@@ -182,40 +216,39 @@ def make_tuple_of_subs(dictionary):
 
 
 def color_text_many(text, subs):
-    # Sort first by the start index (x[0]) and then alphabetically by the substring (x[1])
-    subs.sort(key=lambda x: (x[0], x[1]))  # Sort by index and then by the substring alphabetically
+    """
+    Окрашивание строки если введено несколько подстрок
+    :param text: строка
+    :param subs: подстроки
+    :return: окрашенный текст
+    """
+    subs.sort(key=lambda x: (x[0], x[1]))
     colored_text = ""
     current_index = 0
 
     while subs:
-        start, substring, color = subs.pop(0)  # Extract next substring details
+        start, substring, color = subs.pop(0)
         length = len(substring)
 
-        # Add the plain text between current_index and the start of the next match
         if current_index < start:
             colored_text += text[current_index:start]
             current_index = start
 
-        # Check for overlap with previously colored text
         if current_index > start:
-            overlap = current_index - start  # Calculate overlap length
-            non_overlap_substring = substring[overlap:]  # Get the non-overlapping part of the substring
-            original_substring_part = text[current_index:current_index + len(non_overlap_substring)]
+            overlap = current_index - start
+            non_overlap_substring = substring[overlap:]
+            original_substring_part = \
+                text[current_index:current_index + len(non_overlap_substring)]
 
-            # Color only the remaining part
             colored_text += color + original_substring_part + Style.RESET_ALL
 
-            # Update the current index
             current_index += len(non_overlap_substring)
         else:
-            # No overlap, color the entire substring
             original_substring_part = text[start:start + length]
             colored_text += color + original_substring_part + Style.RESET_ALL
 
-            # Move the current index past the colored substring
             current_index = start + length
 
-    # Add any remaining plain text after the last colored substring
     if current_index < len(text):
         colored_text += text[current_index:]
 
--- search.py
+++ search.py
@@ -1,28 +1,43 @@
+"""
+Модуль реализует функцию поиска подстроки в строке,
+используя алгоритм Бойера-Мура-Хорспула
+"""
+
+
 def search(string, sub_string, case_sensitivity, method, count):
+    """
+    Функция соединяет в себе все фугкции для поиска подстроки в строке
+    :param string: Строка, в которой ведется поиск
+    :param sub_string: Одна или несколько подстрок
+    :param case_sensitivity: Чувствительность к регистру
+    :param method: Поиск с начала или с конца строки
+    :param count: Количество совпадений
+    :return: None или словарь с индексами или кортеж с индексами
+    """
     if isinstance(sub_string, str):
-        sub_string = [sub_string]  # Приводим подстроки к списку, если это одна строка
-    if not case_sensitivity:  # Если регистр не важен, приводим строку и подстроки к нижнему регистру
+        sub_string = [sub_string]
+    if not case_sensitivity:
         sub_string = [i.lower() for i in sub_string]
         string = string.lower()
 
     list_of_subs = []
     for i in sub_string:
-        list_of_subs.append(make_table(i))  # Строим таблицу смещений для каждой подстроки
+        list_of_subs.append(make_table(i))
 
     list_of_finds = []
     if method == 'first':
         for i in range(len(sub_string)):
             list_of_finds.append(
-                boyer_moore_horspool(string, sub_string[i], list_of_subs[i], count))  # Ищем каждую подстроку
+                boyer_moore_horspool(string, sub_string[i],
+                                     list_of_subs[i], count))
     else:
         for i in range(len(sub_string)):
             list_of_finds.append(
-                search_from_end(string, sub_string[i], count))  # Ищем каждую подстроку
+                search_from_end(string, sub_string[i], count))
 
     if len(list_of_finds) == 1:
-        return list_of_finds[0]  # Если одна подстрока, возвращаем результат поиска напрямую
+        return list_of_finds[0]
 
-    # Возвращаем словарь с результатами для всех подстрок
     if all(i is None for i in list_of_finds):
         return None
     dictionary = {}
@@ -32,7 +47,7 @@ def search(string, sub_string, case_sensitivity, method, count):
     return dictionary
 
 
-def boyer_moore_horspool(text: str, pattern: str, shift_dict: dict, count: int):
+def boyer_moore_horspool(text, pattern, shift_dict, count):
     """
     Реализует алгоритм Бойера-Мура-Хорспула для поиска подстроки в строке.
 
@@ -52,20 +67,18 @@ def boyer_moore_horspool(text: str, pattern: str, shift_dict: dict, count: int):
     i = 0
 
     while i <= len_text - len_pattern:
-        # Сравниваем подстроку с конца
         j = len_pattern - 1
         while j >= 0 and pattern[j] == text[i + j]:
             j -= 1
-        # Если все символы совпали, сохраняем индекс
         if j == -1:
             indices.append(i)
             count1 += 1
             if count == count1:
                 return tuple(indices)
-            i += 1  # Смещаем на 1 символ вперед после нахождения подстроки
+            i += 1
         else:
-            # Используем таблицу смещений для пропуска ненужных символов
-            shift = shift_dict.get(text[i + len_pattern - 1], len_pattern)
+            shift = shift_dict.get(text[i + len_pattern - 1],
+                                   len_pattern)
             i += shift
 
     if len(indices) == 0:
@@ -74,36 +87,37 @@ def boyer_moore_horspool(text: str, pattern: str, shift_dict: dict, count: int):
 
 
 def search_from_end(text: str, pattern: str, count: int):
-    """Ищет все вхождения подстроки pattern в строке text с конца текста."""
+    """
+    Функция для поиска подстрок с конца текста
+    :param text: строка
+    :param pattern: подстрока
+    :param count: число вхождений
+    :return: кортеж индексов вхождений
+    """
     len_text = len(text)
     len_pattern = len(pattern)
 
-    # Если подстрока длиннее текста, искать нечего
     if len_pattern > len_text:
         return None
 
-    # Построение таблицы смещений
     shift_dict = build_shift_table(pattern)
 
     count1 = 0
     indices = []
 
-    # Начинаем с конца текста
     i = len_text - len_pattern
     while i >= 0:
-        # Сравниваем символы с начала подстроки, но двигаемся по тексту справа налево
         j = 0
         while j < len_pattern and pattern[j] == text[i + j]:
             j += 1
 
-        if j == len_pattern:  # Если вся подстрока совпала
+        if j == len_pattern:
             indices.append(i)
             count1 += 1
             if count == count1:
                 return tuple(indices)
-            i -= 1  # Смещаемся на 1 символ влево после нахождения совпадения
+            i -= 1
         else:
-            # Используем таблицу смещений для пропуска ненужных символов
             shift = shift_dict.get(text[i], len_pattern)
             i -= shift
 
@@ -114,43 +128,56 @@ def search_from_end(text: str, pattern: str, count: int):
 
 def build_shift_table(pattern: str) -> dict:
     """
-    Создает таблицу смещений для подстроки, смещения строятся зеркально, относительно последнего символа.
+    Создание таблицы смещений для поиска с конца
+    :param pattern: подстрока
+    :return: таблица смещений (словарь)
     """
-    set_pattern = set()  # Набор уникальных символов в подстроке
-    len_pattern = len(pattern)  # Длина подстроки
-    d = {}  # Таблица смещений
+    set_pattern = set()
+    len_pattern = len(pattern)
+    dictionary = {}
 
-    for i in range(1, len_pattern):  # Проходим по подстроке с предпоследнего символа
-        if pattern[i] not in set_pattern:  # Если символ еще не был добавлен в таблицу смещений
-            d[pattern[i]] = i
+    for i in range(1, len_pattern):
+        if pattern[i] not in set_pattern:
+            dictionary[pattern[i]] = i
             set_pattern.add(pattern[i])
 
-    if pattern[0] not in set_pattern:  # Отдельно обрабатываем последний символ подстроки
-        d[pattern[0]] = len_pattern
+    if pattern[0] not in set_pattern:
+        dictionary[pattern[0]] = len_pattern
 
-    d['*'] = len_pattern  # Смещение для всех остальных символов (которых нет в подстроке)
-    return d
-
-
-def make_table(substring):
-    set_of_chars = set()  # Набор уникальных символов в подстроке
-    len_substring = len(substring)  # Длина подстроки
-    dictionary = {}  # Таблица смещений
-
-    for i in range(len_substring - 2, -1, -1):  # Проходим по подстроке с предпоследнего символа
-        if substring[i] not in set_of_chars:  # Если символ еще не был добавлен в таблицу смещений
-            dictionary[substring[i]] = len_substring - i - 1
-            set_of_chars.add(substring[i])
-
-    if substring[len_substring - 1] not in set_of_chars:  # Отдельно обрабатываем последний символ подстроки
-        dictionary[substring[len_substring - 1]] = len_substring
-
-    dictionary['*'] = len_substring  # Смещение для всех остальных символов (которых нет в подстроке)
+    dictionary['*'] = len_pattern
     return dictionary
 
 
-def get_first_n_occurrences(substrings_dict, n, order='first'):
-    # Шаг 1: Собираем все вхождения в виде списка с подстрокой и её индексом
+def make_table(substring):
+    """
+    Таблица смещений для поиска с начала строки
+    :param substring: подсторка
+    :return: таблица смещений (словарь)
+    """
+    set_of_chars = set()
+    len_substring = len(substring)
+    dictionary = {}
+
+    for i in range(len_substring - 2, -1, -1):
+        if substring[i] not in set_of_chars:
+            dictionary[substring[i]] = len_substring - i - 1
+            set_of_chars.add(substring[i])
+
+    if substring[len_substring - 1] not in set_of_chars:
+        dictionary[substring[len_substring - 1]] = len_substring
+
+    dictionary['*'] = len_substring
+    return dictionary
+
+
+def get_first_n_occurrences(substrings_dict, num, order='first'):
+    """
+    Получает словарь со всеми вхождениями и оставляет только первые n
+    :param substrings_dict: словарь вхождений подстрок в строку
+    :param num: количество допустимых вхождений
+    :param order: поиск с начала или с конца
+    :return: словарь
+    """
     all_occurrences = []
     for substring, occurrences in substrings_dict.items():
         if occurrences is None:
@@ -158,28 +185,21 @@ def get_first_n_occurrences(substrings_dict, n, order='first'):
         for index in occurrences:
             all_occurrences.append((substring, index))
 
-    # Шаг 2: Сортируем в зависимости от порядка поиска
     if order == 'first':
-        # Прямой поиск: сортируем по индексу, затем по подстроке
         all_occurrences.sort(key=lambda x: (x[1], x[0]))
     elif order == 'last':
-        # Обратный поиск: сортируем сначала по убыванию индексов, затем по алфавиту
         all_occurrences.sort(key=lambda x: (-x[1], x[0]))
     else:
         raise ValueError("Order must be either 'first' or 'last'.")
 
-    # Шаг 3: Оставляем только первые n вхождений
-    selected_occurrences = all_occurrences[:n]
+    selected_occurrences = all_occurrences[:num]
 
-    # Шаг 4: Формируем новый словарь с теми же ключами
     result = {key: [] for key in substrings_dict}
 
-    # Шаг 5: Заполняем словарь первыми вхождениями
     for substring, index in selected_occurrences:
         result[substring].append(index)
 
-    # Шаг 6: Преобразуем значения в кортежи, как в исходных данных
-    result = {key: tuple(value) if value else None for key, value in result.items()}
+    result = {key: tuple(value) if value else None
+              for key, value in result.items()}
 
     return result
-
